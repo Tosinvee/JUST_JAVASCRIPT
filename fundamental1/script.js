a61f273afb9791faeb0c429e924ed58f4c6561b1
Tosinvee <bayodeoluwatosin06@gmail.com>
2024-04-01 17:02:26 +0100

    data types

---
+++ fundamental1/script.js
@@ -0,0 +1,18 @@
+// let js = "amazing";
+// if (js === "amazing") alert("javascript is fun");
+
+// 48 + 50 + 12;
+// console.log(48 + 50 + 12);
+
+// // VARIABLE NAME CONVENTION
+// let person = 'shade';
+// let PI = 3.415;
+
+// let myFavoriteFood = 'banana'
+// console.log(myFavoriteFood) 
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// to get the type of operators
+console.log(typeof true)
