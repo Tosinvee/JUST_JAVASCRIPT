a3840c5f6053a4a333f813e6d763e45662b4eb1e
Tosinvee <bayodeoluwatosin06@gmail.com>
2024-04-03 22:29:33 +0100

    basic operations

--- fundamental1/script.js
+++ fundamental1/script.js
@@ -11,8 +11,51 @@
 // let myFavoriteFood = 'banana'
 // console.log(myFavoriteFood) 
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
-// to get the type of operators
-console.log(typeof true)
+// // TO GET THE TYPE OF OPERATION
+// console.log(typeof true)
+// console.log(typeof 23)
+
+// javascriptIsFun = 'YES'
+// //REASSIGNING  javascriptIsFun
+// console.log( typeof javascriptIsFun);
+
+// UNDEFINED
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+//VARIABLE DECLARATION
+// let age = 30; //MUTABLE VARIABLE
+// age = 33; // THE VARIABLE WAS MUTATED i.e REASSIGN
+ 
+// const birtYear = 1997; // TO DECLARE A VARIABLE THAT CANT BE REASSIGN
+// //OUTPUT IS ERROR
+// birtYear = 2005;// IMMUTABLE VARIABLE
+
+//MATH OPERATORS
+const ageShade = 2024 - 1997;
+const ageBusayo = 2024 - 1996;
+console.log(ageShade, ageBusayo)
+
+console.log(ageBusayo * 2, ageShade / 2)
+
+//plus operator can be used to concatenate string
+const firstName = 'shade';
+const lastName = 'Bayode';
+console.log(firstName + ' ' + lastName)// adding an empty string to add space betweeen them
+
+//ASSIGNMENT OPERATOR
+let x = 10 + 5; //15
+x += 10 // x = x + 10 = 25
+x *= 4 //100
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x)
+
+//COMPARISON OPERATORS
+console.log(ageBusayo > ageShade) // >,<,<=,>=
+console.log(ageShade >= 18)
+
