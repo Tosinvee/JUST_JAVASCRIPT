73c747dc4daf3b4a6c3c22b1ab013b137a7819e3
Tosinvee <bayodeoluwatosin06@gmail.com>
2024-05-24 16:37:34 +0100

    spread operator

--- Data_structure/script.js
+++ Data_structure/script.js
@@ -1,31 +1,114 @@
-//DESTRUCTING ARRAY
 const resturant = {
     name:'Iya hannah enterprises',
     location:"Gowon Estate",
     categories:['Italian', 'pizzaria', 'vegetarian', 'organic'],
     staterMenu:['focaccia', 'GarlicBread','salad' ],
     mainMenu:['pizza', 'pasta', 'risotto'],
+    openingHours: {
+        thu: {
+            open:12,
+            close:22,
+        },
+        fri: {
+            open:11,
+            close:23,
+        },
+        sat: {
+            open:0,
+            close:24,
+        },
+
+    },
     order: function(starterIndex, mainIndex){
         return [this.staterMenu[starterIndex], this.mainMenu[mainIndex]];
-    }
+    },
+
+    orderDelivery: function({starterIndex, mainIndex, time, address}){
+        console.log(`order received! ${this.staterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to the ${address} at ${time}`)
+    }, 
 };
-const arr = [1,2,3]
 
-const [x,y,z] = arr
-console.log(x,y,z)
-console.log(arr)
-const [first, second] = resturant.categories
-console.log(first,second) 
 
-let [main, ,secondary] =resturant.categories
-console.log(main, secondary)
-//SWITCHING VARIABLES
-const temp = main
-main = secondary
-secondary = temp
-console.log(main, secondary)
+// resturant.orderDelivery({
+//     time:'22:30',
+//     address: 'gowon estate church village',
+//     mainIndex: 2,
+//     starterIndex: 2
+// })
+//DESTRUCTING OBJECTS
+// const {name, openingHours, categories} = resturant
+// console.log(name, openingHours, categories)
 
-//Receives two return value from a function
-console.log(resturant.order(2, 0))
-const [starter, mainCourse] = resturant.order(2, 0)
-console.log(starter, mainCourse)
+// const{
+//     name: resturantName,
+//     openingHours:hours,
+//     categories: tags, 
+// } = resturant;
+// console.log(resturantName, hours, tags)
+// //DEFAULT VALUE
+// const{menu = [], staterMenu:starters =[]} =resturant;
+// console.log(menu, starters)
+//MUTATING VARIABLES
+// let a = 111
+// let b = 999
+// const obj = {a:23, b:7, c:14}
+// ({a, b} = obj)
+// console.log(a, b)
+
+
+
+///////////////////////////////////////////////////////////////////
+// //DESTRUCTING ARRAY`
+// const arr = [1,2,3]
+
+// const [x,y,z] = arr 
+// console.log(x,y,z)
+// console.log(arr)
+// const [first, second] = resturant.categories
+// console.log(first,second) 
+
+// let [main, ,secondary] =resturant.categories
+// console.log(main, secondary)
+// //SWITCHING VARIABLES
+// const temp = main
+// main = secondary
+// secondary = temp
+// console.log(main, secondary)
+
+// //Receives two return value from a function
+// console.log(resturant.order(2, 0))
+// const [starter, mainCourse] = resturant.order(2, 0)
+// console.log(starter, mainCourse)
+
+/////////////////////////////////////////////////////////////////
+//SPREAD OPERATOR
+// const array =[4,5,6]
+// const newArr = [1,2,3,...array]
+// console.log(newArr)// take all element out of the array and writes them manually
+// console.log(1,2,3,array)
+// console.log(...newArr)// logs the individual element of the array
+
+//copy array
+const menu = [...resturant.mainMenu]
+
+//Join 2 arrays
+const menuComb = [...resturant.staterMenu, ...resturant.mainMenu]
+console.log(menuComb)
+
+//spread method on strings
+const name = 'jonas'
+const letters = [...name, ' ', 'S']
+console.log(letters)
+console.log(...name)
+
+//REAL WORLD EXAMPLE
+//objects with spread operator
+const newResturant = {
+    foundedIn: 2008, ...resturant, founder:'Aderonke Bayode'
+}
+console.log(newResturant)
+
+const resturantCopy = {...resturant}
+resturantCopy.name = 'IYA HANNAH ENT'
+console.log(resturantCopy) 
+
